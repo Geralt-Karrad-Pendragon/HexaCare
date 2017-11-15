59dd483d5c55329681e834b49177428f6b491b1c
BOURTHOUMIEUXThomas <33619462+BOURTHOUMIEUXThomas@users.noreply.github.com>
2017-11-15 14:28:19 +0100

    Add files via upload

---
+++ M3/CLEBON/CLEBON/CLEBON.ino
@@ -0,0 +1,11 @@
+int nbAlea;
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));//initialise la séquence aléatoire
+}
+
+void loop() {
+  nbAlea=random(50,150); //construit le nombre aléatoire
+  Serial.println(nbAlea);//affichage du nombre
+  delay(50);//attente
+}
