45ec3b568002072bf27d05ee8d7b1e45968fcae8
BOURTHOUMIEUXThomas <33619462+BOURTHOUMIEUXThomas@users.noreply.github.com>
2017-11-16 17:40:13 +0100

    Add files via upload

---
+++ M3/sketch_nov16a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // Lance une connexion sur le port série.
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // Écrit sur le port série une valeur au format CSV.
+  // Ici on a 'time;pouls'
+  Serial.print(millis()); // Valeur de time
+  Serial.print(";");
+  Serial.print(random(70, 90)); // Simulation du pouls, valeur aléatoire comprise entre 70 et 89.
+  Serial.print("\n");
+ 
+  // Attend 1000ms (1s) avant de relancer loop().
+  delay(1000);
+}
