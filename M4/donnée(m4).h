1abe60e767c9079371860e412378dd28a0e331d1
araubase <33415682+araubase@users.noreply.github.com>
2017-11-17 10:41:30 +0100

    Delete donnée(m4).h

--- M4/donnée(m4).h
+++
@@ -1,9 +0,0 @@
-#ifndef tailleMax
-#define tailleMax 100
-#endif
-struct donnees{
-	long temps;
-	int pouls;
-} typedef donnees;
-
-void ChargementLignes(int nombre_de_ligne, struct donnees **dataTab);
