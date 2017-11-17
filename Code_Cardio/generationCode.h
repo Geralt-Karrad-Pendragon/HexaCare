84ab2282c90767ab1e47af792f00e09e0fdc4c4c
araubase <33415682+araubase@users.noreply.github.com>
2017-11-17 08:42:57 +0100

    Delete generationCode.h

--- Code_Cardio/generationCode.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENERATIONCODE_H
-#define GENERATIONCODE_H
-#include "generationCode.c"
-#include <stdio.h>
-#include <stdlib.h>
-
-void GenerationCode (int iChoixAllumeLed, int iChoixLed, char *arduino);
-
-#endif // GENERATIONCODE_H
