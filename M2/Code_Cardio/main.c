df8d3ba125b4d5e601bff7ed2bfb84f315178293
Geralt-Karrad-Pendragon <33146615+Geralt-Karrad-Pendragon@users.noreply.github.com>
2017-11-17 08:56:28 +0100

    Update main.c

--- M2/Code_Cardio/main.c
+++ M2/Code_Cardio/main.c
@@ -3,24 +3,21 @@
 #include "menu.h"
 #include "menu_led.h"
 #include "generationCode.h"
-//#include "donnees.h"
-//#include "actions.h"
+
 
 int main()
 {
     int iChoixLed = 0;
     int iChoixAllumeLed = 0;
     iChoixAllumeLed = MenuLed();
-    if (iChoixAllumeLed = 7){
+    if (iChoixAllumeLed = 7){ // boucle if pour choisir la LED à allumé si ce mode est enclenché
         iChoixLed = ChoixLED();
     }
     FILE* arduino = NULL;
-    arduino = fopen("param.h", "w+");
-    GenerationCode(iChoixAllumeLed, arduino, iChoixLed);
+    arduino = fopen("../sketch_171113a/main.c__LED_/param.h", "w+");//création et réédition du param.h
+    GenerationCode(iChoixAllumeLed, arduino, iChoixLed);//appelle de la fonction pour l'édition 
     /*FILE* cardio = NULL;
     cardio = fopen("battement.txt", "a");
     menu(cardio);*/
     }
-    /*C:\\Users\\Patricia\\Documents\\Travail\\C.E.S.I\\Projet_Cardio\\sketch_171113a\\main.c__LED_*/
-    //../sketch_171113a/main.c__LED_/param.h
-//C:\Users\Patricia\Documents\Travail\C.E.S.I\Projet_Cardio\sketch_171113a\main.c__LED_
+   
